df30329193ed909b38873475182a90c76cab69cd
alejo1901c <alejandro1901calderon@gmail.com>
2024-04-23 19:05:19 -0500

    primer commit

---
+++ resources/views/bienvenido.blade.php
@@ -0,0 +1,16 @@
+@extends('layout')
+@section('titulo','sobre el carro')
+@section('contenido')
+<p>El Lamborghini Huracán es el acuerdo ideal entre tecnología y diseño.
+    La primera emoción se siente con solo mirarlo. Líneas definidas y aerodinámicas diseñadas para contrarrestar el
+    aire y domar la carretera. La segunda se experimenta al tocarlo. Se emplean las mejores habilidades artesanales
+    italianas para la elaboración de acabados de prestigio y calidad sin precedentes. La tercera emoción se
+    experimenta al pulsar el botón de encendido y sentir el motor V10 de aspiración natural y toda la tecnología
+    necesaria para controlarlo. La última emoción proviene del sistema de iluminación full LED y el salpicadero con
+    TFT de 12,3'', para una experiencia de conducción incomparable.</p>
+@endsection
+<body>
+
+</body>
+
+</html>
---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+@section('titulo','contactanos')
+@section('contenido')
+<h1>contacto</h1>
+<p>escribenos al numero de telefono +57 3103478191</p>
+
+@endsection
+<body>
+
+</body>
+
+</html>
